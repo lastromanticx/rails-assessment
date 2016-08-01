da4594578a434de77903474f1eb4db8589415e43
lastromanticx <giladbarkan@live.com>
2016-08-01 12:30:05 -0400

    added flash message layout

--- assessment/app/controllers/tasks_controller.rb
+++ assessment/app/controllers/tasks_controller.rb
@@ -14,11 +14,19 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find_by(id: params[:id])
-    if @task.nil?
-      not_found
-    elsif @task.list.user != current_user
-      redirect_to user_path(current_user)
+    if params[:list_id]
+      list = List.find_by(id: params[:list_id])
+      if list.nil?
+        flash[:warning] = "List not found."
+        redirect_to user_path(current_user)
+      elsif list.user != current_user
+        redirect_to user_path(current_user)
+      else
+        @task = list.tasks.find_by(id: params[:id])
+        redirect_to list_path(list), alert: "Task not found." if @task.nil?
+      end
+    else
+      @task = task.find(params[:id])
     end
   end
 
